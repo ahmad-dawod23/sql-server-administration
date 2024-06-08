f9f0e5a3a8ec29d2b6df825eda7af33c1addefa1
ahmad-dawod23 <ahmadibrahimdawod@gmail.com>
2024-06-08 19:01:13 +0300

    Update T-SQL commands.sql

--- T-SQL commands.sql
+++ T-SQL commands.sql
@@ -516,5 +516,40 @@ deallocate crsr
 
 
 
+CREATE TABLE [dbo].[tblEmployee](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Gender] [nvarchar](50) NULL,
+	[Salary] [nvarchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO 
 
+
+
+
+CREATE PROCEDURE [dbo].[Sp_AddEmployee]
+	-- Add the parameters for the stored procedure here
+	@Name nvarchar(50),
+	@Gender nvarchar(20),
+	@Salary int
+
+AS
+BEGIN
+
+	INSERT INTO [dbo].[tblEmployee]
+           ([Name]
+           ,[Gender]
+           ,[Salary])
+     VALUES
+           (@Name
+           ,@Gender
+           ,@Salary)
+
+
+
+END
 
